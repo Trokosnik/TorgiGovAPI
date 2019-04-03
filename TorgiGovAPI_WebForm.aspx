8ebd205c018d8297e77c61796f4abe56edb7b25c
Trokosnik <49223116+Trokosnik@users.noreply.github.com>
2019-04-03 10:14:34 +0500

    Add files via upload

---
+++ TorgiGovAPI_WebForm.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="TorgiGovAPI_WebForm.aspx.cs" Inherits="TorgiGovAPI_WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <div>Получение данных с сайта https://torgi.gov.ru/</div><br />
+    <form id="form1" runat="server">
+        <select id="bidKind" runat="server">
+            <option value="0">--выбрать вид торгов--</option>
+            <option value="1">1 - Аренда, безвозмездное пользование, доверительное управление имуществом, иные договоры..</option>
+            <option value="2">2 - Аренда и продажа земельных участков</option>
+            <option value="3">3 - Строительство</option>
+            <option value="4">4 - Охотхозяйственные соглашения</option>
+            <option value="5">5 - Пользование участками недр</option>
+            <option value="6">6 - Государственно-частное партнерство (соглашения о ГЧП/МЧП, концессионные соглашения)</option>
+            <option value="7">7 - Аренда лесных участков и продажа лесных насаждений</option>
+            <option value="8">8 - Продажа государственного и муниципального имущества</option>
+            <option value="9">9 - Передача прав на единые технологии</option>
+            <option value="10">10 - Водопользование</option>
+            <option value="11">11 - Рыболовство и добыча водных биоресурсов</option>
+            <option value="12">12 - ЖКХ</option>
+            <option value="13">13 - Реализация имущества должников</option>
+            <option value="14">14 - Создание искусственных земельных участков</option>
+            <option value="15">15 - Размещение рекламных конструкций</option>
+            <option value="16">16 - Продажа объектов электроэнергетики</option>
+            <option value="17">17 - Лицензии на оказание услуг связи</option>
+        </select>
+        <br />
+        от: <asp:Calendar ID="publishDateFrom" runat="server"></asp:Calendar>
+        до: <asp:Calendar ID="publishDateTo" runat="server"></asp:Calendar>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Старт" />
+    </form>
+</body>
+</html>
---
+++ TorgiGovAPI_WebForm.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class TorgiGovAPI_WebForm : System.Web.UI.Page
+{
+    /*protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }*/
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (Convert.ToInt32(bidKind.Value) != 0)
+        {
+            //String URLString = "http://localhost:50689/books.xml";
+        
+            //String URLString = "http://localhost:50689/data-" + bidKind.Value + "-" + publishDateFrom.SelectedDate.ToString("yyyyMMddTHHmm") + "-" + publishDateTo.SelectedDate.ToString("yyyyMMddTHHmm") + "-structure-20130401T0000.xml";
+
+            String URLString = "http://torgi.gov.ru/opendata/7710349494-torgi/data-" + bidKind.Value + "-" + publishDateFrom.SelectedDate.ToString("yyyyMMddTHHmm") + "-" + publishDateTo.SelectedDate.ToString("yyyyMMddTHHmm") + "-structure-20130401T0000.xml";
+            
+            //Response.Write(URLString);
+            
+            XmlTextReader reader = new XmlTextReader(URLString);
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(URLString);
+
+            XmlElement xRoot = xDoc.DocumentElement;
+
+            Response.Write("<table border='1' width='1250'>");
+            Response.Write("<tr><td>Наименование типа выгрузки</td><td>Номер</td><td>Наименовение организации</td><td>Публикация</td><td>Изменен</td><tr>");
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xRoot)
+            {
+                // получаем атрибут name
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
+                    if (attr != null)
+                        Console.WriteLine(attr.Value);
+                }
+                // обходим все дочерние узлы элемента user
+                Response.Write("<tr>");
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "bidKindName")
+                    {
+                        Response.Write("<td>" + childnode.InnerText+"</td>");
+                    }
+                    if (childnode.Name == "bidNumber")
+                    {
+                        Response.Write("<td>" + childnode.InnerText + "</td>");
+                    }
+                    if (childnode.Name == "organizationName")
+                    {
+                        Response.Write("<td>" + childnode.InnerText + "</td>");
+                    }
+                    if (childnode.Name == "publishDate")
+                    {
+                        Response.Write("<td>" + childnode.InnerText + "</td>");
+                    }
+                    if (childnode.Name == "lastChanged")
+                    {
+                        Response.Write("<td>" + childnode.InnerText + "</td>");
+                    }
+                }
+                Response.Write("</tr>");
+            }
+            Response.Write("</table>");
+
+        }
+    }
+ }
